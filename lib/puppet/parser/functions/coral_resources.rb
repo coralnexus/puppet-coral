92535bbe041603fe814c6f7a33b3aa012bbdde66
Adrian Webb <adrian.webb@coraltech.net>
2013-04-03 01:52:25 -0400

    Reworking the coral_resources() function and resource library to make it more uniformally extensible in both overrides and defaults and adding resource prefixes.

--- lib/puppet/parser/functions/coral_resources.rb
+++ lib/puppet/parser/functions/coral_resources.rb
@@ -12,7 +12,7 @@
 require File.join(File.dirname(__FILE__), '..', '..', '..', 'resource')
 require 'pp'
 
-module Puppet::Parser::Functions
+module Puppet::Parser::Functions  
   newfunction(:coral_resources, :doc => <<-EOS
 This function adds resource definitions of a specific type to the Puppet catalog
 - Requires
@@ -28,56 +28,67 @@ If no resources are found, it returns without creating anything.
       "given (#{args.size} for 1)") if args.size < 1
       
     definition_name = args[0]
+    type_name       = definition_name.sub(/^\@?\@/, '')
     var_name        = ( args[1] ? args[1] : definition_name )
     default_values  = ( args[2] ? args[2] : {} )
-    tag             = ( args[3] ? args[3] : '' )
     
+    tag             = ( args[3] ? args[3] : '' )
+    tag_var         = tag.empty? ? '' : tag.gsub(/\_/, '::')
+    
+    override_var    = tag_var.empty? ? nil : "#{tag_var}::#{type_name}"
+    default_var     = tag_var.empty? ? nil : "#{tag_var}::#{type_name}_defaults"
+        
     puts "coral_resources -> #{definition_name}"
     #puts var_name.class
     #pp var_name
     #pp default_values
     
-    resource_names  = []
+    parse = lambda do |data, override|
+      results = {}
     
-    case var_name
-    when String
-      resources = function_global_hash([ var_name, {} ])
-      
-    when Array
-      resources = {}
-      var_name.each do |item|
-        case item
+      if override
+        case data
         when String
-          item = function_global_hash([ item, {} ])
-        end
-        if ! item.empty?
-          resources = function_deep_merge([ resources, item ])          
+          data = [ data, override ] if data != override
+        when Array
+          data << override unless data.include?(override)
+        when Hash
+          data = [ data, override ]
         end
       end
     
-    when Hash
-      resources = var_name
-    end
-    
-    case default_values
-    when String
-      default_values = function_global_hash([ default_values, {} ])
+      case data
+      when String
+        results = function_global_hash([ data, {} ])
       
-    when Array
-      defaults = {}
-      default_values.each do |item|
-        case item
-        when String
-          item = function_global_hash([ item, {} ])
+      when Array
+        data.each do |item|
+          if item.is_a?(String)
+            item = function_global_hash([ item, {} ])
+          end
+          if ! item.empty?
+            results = function_deep_merge([ results, item ])          
+          end
         end
-        if ! item.empty?
-          defaults = function_deep_merge([ defaults, item ])          
-        end
+  
+      when Hash
+        results = data
       end
-      default_values = defaults
+      return results
     end
     
-    #pp resources.keys
+    #---
+    # Resources
+    
+    resources = parse.call(var_name, override_dir)
+    
+    #---
+    # Defaults
+    
+    default_values = parse.call(default_values, default_var)
+    
+    #---
+    # Resource processing
     
     resources = Coral::Resource.normalize(definition_name, resources)
     
@@ -86,7 +97,7 @@ If no resources are found, it returns without creating anything.
         unless data.empty?
           resources[name] = function_deep_merge([ default_values, data ])
           
-          if tag
+          unless tag.empty?
             if ! data.has_key?('tag')
               resources[name]['tag'] = tag
             else
@@ -104,7 +115,10 @@ If no resources are found, it returns without creating anything.
       end
       #pp resources
       
-      resources = Coral::Resource.translate(definition_name, function_value([ resources ]))
+      resources = Coral::Resource.translate(definition_name, function_value([ resources ]), tag)
+      
+      #---
+      # Resource creation
       
       function_coral_create_resources([ definition_name, resources ])
     end
--- lib/resource.rb
+++ lib/resource.rb
@@ -2,26 +2,6 @@
 module Coral
 module Resource
   
-  def self.type_name(value)
-    return :main if value == :main
-    return "Class" if value == "" or value.nil? or value.to_s.downcase == "component"
-    return value.to_s.split("::").collect { |s| s.capitalize }.join("::")
-  end
-  
-  #---
-  
-  def self.namevar(type_name, resource_name)
-    resource = Puppet::Resource.new(type_name.sub(/^\@?\@/, ''), resource_name)
-    
-    if resource.builtin_type? and type = resource.resource_type and type.key_attributes.length == 1
-      return type.key_attributes.first.to_s
-    else
-      return 'name'
-    end
-  end
-  
-  #---
-  
   def self.normalize(type_name, resources)
     unless ! resources || resources == :undefined || resources.empty?
       resources.keys.each do |name|
@@ -50,29 +30,24 @@ module Resource
   
   #---
   
-  def self.normalize_keys(hash)
-    result = {}
-    hash.each do |key, value|
-      result[key.to_s] = value
-    end
-    return result
-  end
-  
-  #---
-  
-  def self.translate(type_name, resources)    
+  def self.translate(type_name, resources, prefix = '')    
     resources.each do |name, data|
-      resources[name]['before']    = translate_resource_refs(type_name, data['before']) if data.has_key?('before')
-      resources[name]['notify']    = translate_resource_refs(type_name, data['notify']) if data.has_key?('notify')
-      resources[name]['require']   = translate_resource_refs(type_name, data['require']) if data.has_key?('require')       
-      resources[name]['subscribe'] = translate_resource_refs(type_name, data['subscribe']) if data.has_key?('subscribe')      
+      resources[name]['before']    = translate_resource_refs(type_name, data['before'], prefix) if data.has_key?('before')
+      resources[name]['notify']    = translate_resource_refs(type_name, data['notify'], prefix) if data.has_key?('notify')
+      resources[name]['require']   = translate_resource_refs(type_name, data['require'], prefix) if data.has_key?('require')       
+      resources[name]['subscribe'] = translate_resource_refs(type_name, data['subscribe'], prefix) if data.has_key?('subscribe')
+      
+      unless prefix.empty?
+        resources["#{prefix}-#{name}"] = resources[name]
+        resources.delete(name)
+      end
     end
     return resources
   end
   
   #---
   
-  def self.translate_resource_refs(type_name, resource_refs)
+  def self.translate_resource_refs(type_name, resource_refs, prefix = '')
     return :undef unless resource_refs && resource_refs != :undef && ! resource_refs.empty? && resource_refs != 'undef'
     
     type_name = type_name.sub(/^\@?\@/, '')
@@ -81,6 +56,11 @@ module Resource
         
     if resource_refs.is_a?(String)
       resource_refs = resource_refs.split(/\s*,\s*/)
+      unless prefix.empty?
+        resource_refs.collect! do |value|
+          "#{prefix}-#{value}"  
+        end
+      end
     end
     resource_refs.each do |ref|
       if ! ref.is_a?(Puppet::Resource)
@@ -90,6 +70,36 @@ module Resource
     end
     return values[0] if values.length == 1
     return values
+  end
+  
+  #---
+  
+  def self.type_name(value) # Basically borrowed from Puppet (damn private methods!)
+    return :main if value == :main
+    return "Class" if value == "" or value.nil? or value.to_s.downcase == "component"
+    return value.to_s.split("::").collect { |s| s.capitalize }.join("::")
+  end
+  
+  #---
+  
+  def self.namevar(type_name, resource_name) # Basically borrowed from Puppet (damn private methods!)
+    resource = Puppet::Resource.new(type_name.sub(/^\@?\@/, ''), resource_name)
+    
+    if resource.builtin_type? and type = resource.resource_type and type.key_attributes.length == 1
+      return type.key_attributes.first.to_s
+    else
+      return 'name'
+    end
+  end
+    
+  #---
+  
+  def self.normalize_keys(hash)
+    result = {}
+    hash.each do |key, value|
+      result[key.to_s] = value
+    end
+    return result
   end
 end
 end

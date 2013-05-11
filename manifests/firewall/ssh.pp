7e76f302e301c5544c0b615b18bb1e04b06eccd7
Adrian Webb <adrian.webb@coraltech.net>
2013-05-10 20:49:10 -0400

    Fixing incorrect parameter names in the ssh firewall class.

--- manifests/firewall/ssh.pp
+++ manifests/firewall/ssh.pp
@@ -5,12 +5,12 @@ class coral::firewall::ssh {
   coral::firewall { "${base_name}_firewall_ssh":
     resources => {
       rule => {
-        name   => $coral::params::firewall_ssh_name,
+        name   => $coral::params::ssh::firewall_name,
         action => 'accept',
         chain  => 'INPUT',
         state  => 'NEW',
         proto  => 'tcp',
-        dport  => $coral::params::ssh_port
+        dport  => $coral::params::ssh::port
       }
     }
   }

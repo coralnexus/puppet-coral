763971deb73fa42e495f32bcc91953d19129e1c0
Adrian Webb (local admin) <adrian.webb@coralnexus.com>
2014-03-21 23:49:51 -0400

    Reworking the sshconf template provider to support client configurations as well as server.

--- lib/CORL/template/sshconf.rb
+++ lib/CORL/template/sshconf.rb
@@ -8,21 +8,52 @@ class Sshconf < CORL.plugin_class(:template)
    
   def render_processed(input)
     output = ''
+    hosts  = {}
        
     case input      
     when Hash
       input.each do |name, value|
-        if value.is_a?(Array)
-          value.each do |item|
-            output << "#{name} #{item}\n"  
-          end
-          
-        elsif value.is_a?(String)
-          output << "#{name} #{value}\n"
+        # Only valid on client configs but similar enough in syntax that 
+        # we share the template
+        if name.match(/Host/i) && value.is_a?(Hash)
+          # Defer rendering until the end
+          hosts = value
+        else
+          output << render_configuration(name, value)
         end
       end
+      output << "\n"
+      
+      hosts.each do |host, host_config|
+        output << "Host #{host}\n"
+        host_config.each do |name, value|
+          output << render_configuration(name, value, '  ')
+        end
+        output << "\n"  
+      end
     end              
-    return output      
+    output      
+  end
+  
+  #---
+  
+  def render_configuration(name, value, padding = '')
+    output = ''
+    
+    if value.is_a?(Array)
+      value.each do |item|
+        output << render_statement(name, item, padding) 
+      end          
+    elsif value.is_a?(String)
+      output << render_statement(name, value, padding)
+    end
+    output  
+  end
+  
+  #---
+  
+  def render_statement(name, value, padding = '')
+    "#{padding}#{name} #{value.strip}\n"
   end
 end
 end

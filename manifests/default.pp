d311feb526736a20ceb36336f1bc5014ebe1ef4c
Adrian Webb <adrian.webb@coraltech.net>
2014-04-24 22:04:23 -0400

    Adjusting the default parameters; base packages, Ruby 1.9.1, and splitting Puppet params by version.

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,40 +2,37 @@
 class corl::default {
 
   case $::operatingsystem {
-    debian, ubuntu : {
-      # Debian Common
+    ubuntu : {
+      # Ubuntu Common
 
-      $build_package_names  = ['build-essential', 'libnl-dev', 'libpopt-dev', 'libxml2-dev', 'libssl-dev', 'libcurl4-openssl-dev', 'libxslt1-dev']
-      $common_package_names = ['vim', 'unzip', 'curl']
+      $build_package_names  = ['build-essential', 'libnl-dev', 'libpopt-dev', 'libssl-dev', 'libcurl4-openssl-dev', 'libxslt-dev', 'libxml2-dev']
+      $common_package_names = ['python-software-properties', 'vim', 'unzip', 'curl']
 
-      $package_helper       = 'python-software-properties'
+      # Ubuntu Ruby
 
-      # Debian Ruby
-
-      $ruby_package_names       = ['ruby2.0', 'ruby2.0-dev']
+      $ruby_package_names       = ['ruby1.9.1', 'ruby1.9.1-dev']
       $ruby_extra_package_names = []
 
-      $ruby_exec     = '/usr/bin/ruby2.0'
-      $rubygems_exec = '/usr/bin/gem2.0'
-      $gem_home      = '/var/lib/gems/2.0.0'
+      $ruby_exec     = '/usr/bin/ruby1.9.1'
+      $rubygems_exec = '/usr/bin/gem1.9.1'
+      $gem_home      = '/var/lib/gems/1.9.1'
 
       $ruby_env_file        = '/etc/profile.d/ruby.sh'
       $ruby_root_gemrc_file = '/root/.gemrc'
 
-      $ruby_set_repo_command       = "add-apt-repository -y ppa:brightbox/ruby-ng-experimental"
-      $ruby_package_update_command = 'apt-get update'
+      $ruby_set_repo_command       = undef
+      $ruby_package_update_command = undef
 
-      # Debian Puppet
+      # Ubuntu Puppet
 
       $puppet_package_name        = 'puppet'
-      $puppet_package_ensure      = '3.4.3-1puppetlabs1'
-      $puppet_extra_package_names = ['vim-puppet']
+      $puppet_extra_package_names = []
 
       $puppet_init_config_file = '/etc/default/puppet'
 
       $puppet_bin = '/usr/bin/puppet'
 
-      # Debian SSH
+      # Ubuntu SSH
 
       $ssh_package_names       = ['openssh-server']
       $ssh_extra_package_names = ['ssh-import-id']
@@ -45,7 +42,7 @@ class corl::default {
       $sshd_config_file = '/etc/ssh/sshd_config'
       $ssh_config_file  = '/etc/ssh/ssh_config'
 
-      # Debian Sudo
+      # Ubuntu Sudo
 
       $visudo_bin = '/usr/sbin/visudo'
 
@@ -54,9 +51,20 @@ class corl::default {
 
       $sudoers_dir = '/etc/sudoers.d'
 
-      # Debian Coral logging
+      # Ubuntu CORL logging
 
       $log_dir = '/var/log/corl'
+
+      # Ubuntu release diferentiated properties
+
+      case $::operatingsystemrelease {
+        /^1[23].\d+$/: {
+          $puppet_package_ensure = '3.4.3-1puppetlabs1'
+        }
+        /^14.\d+$/: {
+          $puppet_package_ensure = '3.4.3-1'
+        }
+      }
     }
   }
 }

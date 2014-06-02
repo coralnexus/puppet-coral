fcceb862daddb2c2086ef3795d93cab5ef706b02
Adrian Webb <adrian.webb@coraltech.net>
2014-06-02 03:47:19 -0400

    Updating the nucleon template plugin providers to use the updated plugin architecture.

--- lib/nucleon/template/sshconf.rb
+++ lib/nucleon/template/sshconf.rb
@@ -1,7 +1,7 @@
 
-module CORL
+module Nucleon
 module Template
-class Sshconf < CORL.plugin_class(:template)
+class Sshconf < CORL.plugin_class(:nucleon, :template)
 
   #-----------------------------------------------------------------------------
   # Renderers  
